209e17820acbab9c80e6c0545ccbfd9e0d0106b6
flavopila <faisal.salw@gmail.com>
2026-03-12 20:36:41 +0000

    feat: create short exercise library in javascript

---
+++ assets/js/library.js
@@ -0,0 +1,75 @@
+const exerciseLibrary = [
+  {
+    name: "Barbell Back Squat",
+    type: "compound",
+    primaryMuscleGroup: "quadriceps",
+    secondaryMuscleGroup: "glutes, hamstrings",
+    equipment: "free weight"
+  },
+  {
+    name: "Barbell Bench Press",
+    type: "compound",
+    primaryMuscleGroup: "chest",
+    secondaryMuscleGroup: "triceps, front deltoids",
+    equipment: "free weight"
+  },
+  {
+    name: "Conventional Deadlift",
+    type: "compound",
+    primaryMuscleGroup: "hamstrings",
+    secondaryMuscleGroup: "glutes, lower back",
+    equipment: "free weight"
+  },
+  {
+    name: "Overhead Press",
+    type: "compound",
+    primaryMuscleGroup: "shoulders",
+    secondaryMuscleGroup: "triceps, upper chest",
+    equipment: "free weight"
+  },
+  {
+    name: "Pull‑Up",
+    type: "compound",
+    primaryMuscleGroup: "lats",
+    secondaryMuscleGroup: "biceps, upper back",
+    equipment: "bodyweight"
+  },
+  {
+    name: "One‑Arm Dumbbell Row",
+    type: "compound",
+    primaryMuscleGroup: "lats",
+    secondaryMuscleGroup: "mid back, biceps",
+    equipment: "free weight"
+  },
+  {
+    name: "Romanian Deadlift",
+    type: "compound",
+    primaryMuscleGroup: "hamstrings",
+    secondaryMuscleGroup: "glutes, lower back",
+    equipment: "free weight"
+  },
+  {
+    name: "Dumbbell Lateral Raise",
+    type: "isolation",
+    primaryMuscleGroup: "side deltoids",
+    secondaryMuscleGroup: "upper traps",
+    equipment: "free weight"
+  },
+  {
+    name: "Cable Triceps Pushdown",
+    type: "isolation",
+    primaryMuscleGroup: "triceps",
+    secondaryMuscleGroup: "none",
+    equipment: "machine"
+  },
+  {
+    name: "Dumbbell Biceps Curl",
+    type: "isolation",
+    primaryMuscleGroup: "biceps",
+    secondaryMuscleGroup: "forearms",
+    equipment: "free weight"
+  }
+];
+
+
+
