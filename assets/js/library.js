1922dbdbc6375e0702f313a5b402f9cad19dd575
flavopila <faisal.salw@gmail.com>
2026-03-15 21:50:47 +0000

    feat/refactor: begin creating layout for library, reduce redundant CSS code & adjust pathing in library.js

--- assets/js/library.js
+++ assets/js/library.js
@@ -4,69 +4,79 @@ export const exerciseLibrary = [
     type: "compound",
     primaryMuscleGroup: "quadriceps",
     secondaryMuscleGroup: "glutes, hamstrings",
-    equipment: "free weight"
+    equipment: "free weight",
+    image: "../img/lifts/barbell-back-squat.webp"
   },
   {
     name: "Barbell Bench Press",
     type: "compound",
     primaryMuscleGroup: "chest",
     secondaryMuscleGroup: "triceps, front deltoids",
-    equipment: "free weight"
+    equipment: "free weight",
+    image: "../img/lifts/barbell-bench-press.webp"
   },
   {
     name: "Conventional Deadlift",
     type: "compound",
     primaryMuscleGroup: "hamstrings",
     secondaryMuscleGroup: "glutes, lower back",
-    equipment: "free weight"
+    equipment: "free weight",
+    image: "../img/lifts/conventional-deadlift.webp"
   },
   {
     name: "Overhead Press",
     type: "compound",
     primaryMuscleGroup: "shoulders",
     secondaryMuscleGroup: "triceps, upper chest",
-    equipment: "free weight"
+    equipment: "free weight",
+    image: "../img/lifts/overhead-press.webp"
   },
   {
     name: "Pullup",
     type: "compound",
     primaryMuscleGroup: "lats",
     secondaryMuscleGroup: "biceps, upper back",
-    equipment: "bodyweight"
+    equipment: "bodyweight",
+    image: "../img/lifts/pullup.webp"
   },
   {
     name: "Dumbbell Row",
     type: "compound",
     primaryMuscleGroup: "lats",
     secondaryMuscleGroup: "mid back, biceps",
-    equipment: "free weight"
+    equipment: "free weight",
+    image: "../img/lifts/dumbbell-row.webp"
   },
   {
     name: "Romanian Deadlift",
     type: "compound",
     primaryMuscleGroup: "hamstrings",
     secondaryMuscleGroup: "glutes, lower back",
-    equipment: "free weight"
+    equipment: "free weight",
+    image: "../img/lifts/romanian-deadlift.webp"
   },
   {
     name: "Dumbbell Lateral Raise",
     type: "isolation",
     primaryMuscleGroup: "side deltoids",
     secondaryMuscleGroup: "upper traps",
-    equipment: "free weight"
+    equipment: "free weight",
+    image: "../img/lifts/dumbbell-lateral-raise.webp"
   },
   {
     name: "Cable Triceps Pushdown",
     type: "isolation",
     primaryMuscleGroup: "triceps",
     secondaryMuscleGroup: "none",
-    equipment: "machine"
+    equipment: "machine",
+    image: "../img/lifts/cable-triceps-pushdown.webp"
   },
   {
     name: "Dumbbell Biceps Curl",
     type: "isolation",
     primaryMuscleGroup: "biceps",
     secondaryMuscleGroup: "forearms",
-    equipment: "free weight"
+    equipment: "free weight",
+    image: "../img/lifts/dumbbell-biceps-curl.webp"
   }
 ];
